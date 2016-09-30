dd0d603bc61b78f12f1390e363d0b0483caa0491
Orta Therox <orta.therox@gmail.com>
2016-09-30 13:41:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# React Native Storybooks
+
+Adds a command to inline show the Storybook page inline inside VS Code. 
+
+Basically jams http://localhost:9001 into a tab that you can have on the side.
+
+## Features
+
+![https://github.com/orta/vscode-react-native-storybooks/raw/master/preview.png](https://github.com/orta/vscode-react-native-storybooks/raw/master/preview.png)
+
+* Doesn't quite fit initially - you'll have to resize it, there's no extension API for width
+* Gets a bit conflict-y if you still have the page open in   
+* Does work though!
+* So, you got that.
+
+## Known Issues
+
+Nothing critical ATM.
---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+'use strict';
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    let previewUri = vscode.Uri.parse('storybook://authority/preview');
+    class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
+        public provideTextDocumentContent(uri: vscode.Uri): string {
+            return `
+            <style>iframe {
+                position: fixed;
+                border: none;
+                top: 0; right: 0;
+                bottom: 0; left: 0;
+                width: 100%;
+                height: 100%;
+            }
+            </style>
+
+            <body onload="iframe.document.head.appendChild(ifstyle)" style="background-color:red;margin:0px;padding:0px;overflow:hidden">
+                <iframe src="http://localhost:9001" frameborder="0"></iframe>
+                <p>If you're seeing this, something is wrong :) </p>
+            </body>
+            `
+        }
+    }
+
+    let provider = new TextDocumentContentProvider();
+    let registration = vscode.workspace.registerTextDocumentContentProvider('storybook', provider);
+
+    // Registers the storyboards command to trigger a new HTML preview which hosts the storybook server 
+    let disposable = vscode.commands.registerCommand('extension.showStorybookPreview', () => {
+        return vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'Storybooks').then((success) => {
+        }, (reason) => {
+            vscode.window.showErrorMessage(reason);
+        });
+    });
+
+    context.subscriptions.push(disposable, registration);
+}
+
+export function deactivate() {
+}
